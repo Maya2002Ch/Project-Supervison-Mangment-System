8831699c82e715b84c0badbba0697a80a351d6db
warunahello@gmail.com <89479303+Maya2002Ch@users.noreply.github.com>
2023-04-28 03:11:11 +0530

    Updated

---
+++ Supervison_system/src/test/java/com/example/supervison_system/SupervisonSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supervison_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisonSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
